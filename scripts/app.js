9ea37ac1b14e8b334801e7e94b4f6e3e146210a1
davidmonterrosa <dmonterrosa99@gmail.com>
2025-02-11 11:49:24 -0800

    Added function to display current list of names

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,36 @@ let listOfNames = [];
 
 // Functions
 function displayListOfNames() {
-    
+    let namesList = getFromLocalStorage();
+    namesDisplayArea.innerHTML = "";
+    namesList.map((personName) => {
+        let personCard = document.createElement('div');
+        personCard.className = "flex justify-between items-center rounded-2xl bg-slate-600 px-6"
+
+        let nameOfPerson = document.createElement('h1');
+        nameOfPerson.className = "text-white text-2xl";
+        nameOfPerson.innerText = personName;
+
+        let removePersonBtn = document.createElement('button');
+        removePersonBtn.className = "w-8";
+
+        removePersonBtn.addEventListener("click", () => {
+            removeFromLocalStorage(nameOfPerson.innerText);
+            personCard.remove();
+        });
+
+        let deleteIcon = document.createElement('img');
+        deleteIcon.src = "./assets/icons/delete.png";
+        deleteIcon.alt = "Remove name from list";
+
+        namesDisplayArea.appendChild(personCard);
+
+        personCard.appendChild(nameOfPerson);
+        personCard.appendChild(removePersonBtn);
+
+        removePersonBtn.appendChild(deleteIcon);
+
+    })
 }
 
 // Event Listeners
@@ -20,4 +49,8 @@ addName.addEventListener("click", () => {
     listOfNames.push(inputFieldForName.value);
     saveToLocalStorage(inputFieldForName.value);
     console.log(listOfNames);
+    displayListOfNames();
 });
+
+// On Load of site
+displayListOfNames();
