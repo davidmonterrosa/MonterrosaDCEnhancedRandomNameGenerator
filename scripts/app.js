9ceabc939806134165c1882692b2bcc60f31c41f
davidmonterrosa <dmonterrosa99@gmail.com>
2025-02-11 16:27:34 -0800

    Added button functions

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,24 @@ import { saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage } from 
 const inputFieldForName = document.getElementById("inputFieldForName");
 const addName = document.getElementById("addName");
 const namesDisplayArea = document.getElementById("namesDisplayArea");
+const getRandomName = document.getElementById("getRandomName");
+const rngNameResult = document.getElementById("rngNameResult");
+const generateDifferentName = document.getElementById("generateDifferentName");
+const setPeoplePerGroup = document.getElementById("setPeoplePerGroup");
+const setPeoplePerGroupBtn = document.getElementById("setPeoplePerGroupBtn");
+const makeNumberGroups = document.getElementById("makeNumberGroups");
+const makeNumberGroupsBtn = document.getElementById("makeNumberGroupsBtn");
+const total = document.getElementById("total");
+
 
 
 // Variables
-let listOfNames = [];
+let listOfNames = getFromLocalStorage();
 
 
 // Functions
 function displayListOfNames() {
+    total.innerText = `Number of people: ${listOfNames.length}`;
     let namesList = getFromLocalStorage();
     namesDisplayArea.innerHTML = "";
     namesList.map((personName) => {
@@ -27,7 +37,12 @@ function displayListOfNames() {
 
         removePersonBtn.addEventListener("click", () => {
             removeFromLocalStorage(nameOfPerson.innerText);
+
+            let idToRemove = listOfNames.indexOf(nameOfPerson.innerText);
+            listOfNames.splice(idToRemove, 1);
+        
             personCard.remove();
+            console.log(listOfNames);
         });
 
         let deleteIcon = document.createElement('img');
@@ -44,12 +59,113 @@ function displayListOfNames() {
     })
 }
 
+function pickRandomName() {
+    let randomIndex = Math.floor(Math.random() * listOfNames.length);
+    return listOfNames[randomIndex];
+}
+
+
+// The Following code was taken from ChatGPT 4o mini Web Edition w/o account on Feb 11, 2025
+// and Isaiah Ferguson unity lecture on Fisher-Yates shuffle method
+function randomizeArray(listOfNames) {
+    // Create a copy of the original array to avoid mutating it
+    let arrayCopy = [...listOfNames];
+  
+    // Fisher-Yates (Knuth) Shuffle
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // Swap elements
+      [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+    }
+  
+    return arrayCopy;
+}
+// End of Code Block  
+
+function groupByNumberOfPeople(people) {
+    // I want to iterate through the array
+    // For every (people) items in the array
+    // Create Group  
+    const arrCopy = randomizeArray(listOfNames);
+    let temporaryArray = [];
+    let twoDArray = [];
+    let counter = 0;
+    for(let i = 0; i < listOfNames.length; i++) {
+        if(counter == people) {
+            twoDArray.push(temporaryArray);
+            console.log(twoDArray);
+            temporaryArray = [];
+            counter = 0;
+        } else {
+            temporaryArray.push(arrCopy[i]);
+            counter++;
+        }
+    }
+    if(counter == 1) {
+        twoDArray[twoDArray.length - 1].push(temporaryArray[0]);
+    }
+    if(counter > 1) {
+        console.log(temporaryArray);
+        twoDArray.push(temporaryArray);
+    }
+
+}
+
+function groupByNumberOfGroups(groups) {
+    // take number of names (listOfNames.length) and divide by groups
+    // This gives number of people per group
+    const arrCopy = randomizeArray(listOfNames);
+    let peoplePerGroup = parseInt(arrCopy.length / groups);
+    console.log(peoplePerGroup);
+    let temporaryArray = [];
+    let twoDArray = [];
+    let counter = 0;
+    for(let i = 0; i < listOfNames.length; i++) {
+        if(counter == peoplePerGroup) {
+            twoDArray.push(temporaryArray);
+            console.log(twoDArray);
+            temporaryArray = [];
+            counter = 0;
+        } else {
+            temporaryArray.push(arrCopy[i]);
+            counter++;
+        }
+    }
+    if(counter == 1) {
+        twoDArray[twoDArray.length - 1].push(temporaryArray[0]);
+    }
+    if(counter > 1) {
+        console.log(temporaryArray);
+        twoDArray.push(temporaryArray);
+    }
+}
+
 // Event Listeners
 addName.addEventListener("click", () => {
-    listOfNames.push(inputFieldForName.value);
+    if(!listOfNames.includes(inputFieldForName.value)) {
+        listOfNames.push(inputFieldForName.value);
+    }
     saveToLocalStorage(inputFieldForName.value);
     console.log(listOfNames);
     displayListOfNames();
+    total.innerText = `Number of people: ${listOfNames.length}`;
+});
+
+getRandomName.addEventListener("click", () => {
+    rngNameResult.innerText = pickRandomName();
+});
+
+generateDifferentName.addEventListener("click", () => {
+    rngNameResult.innerText = pickRandomName();
+    console.log(rngNameResult.innerText);
+});
+
+setPeoplePerGroupBtn.addEventListener("click", () => {
+    groupByNumberOfPeople(parseInt(setPeoplePerGroup.value))
+});
+
+makeNumberGroupsBtn.addEventListener("click", () => {
+    groupByNumberOfGroups(parseInt(makeNumberGroups.value))
 });
 
 // On Load of site
